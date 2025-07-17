15cf274d7b705d91ab18e102f2c28e6ce6dca275
Aziz Kammoun <azizkammoun@Azizs-MacBook-Pro.local>
2025-07-17 11:43:06 +0100

    somme fixes

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cannes">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/profil" element={<ProfilePage />} />
