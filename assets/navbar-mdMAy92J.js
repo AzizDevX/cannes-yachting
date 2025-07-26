bea0d494d9fa089d1ec6a34ba45017309b78857d
AzizDevX <azizkammoun1919@gmail.com>
2025-07-26 17:03:48 +0100

    Updates

--- assets/navbar-mdMAy92J.js
+++ assets/navbar-mdMAy92J.js
@@ -1,4 +1,4 @@
-import{r as i,j as e,L as n}from"./index-DGhTzKiO.js";import{N as k}from"./footer-BsyUvFY4.js";/**
+import{r as i,j as e,L as n}from"./index-Nu14hnaC.js";import{N as k}from"./footer-B8-s_gDL.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
