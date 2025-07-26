5d9620f41293ab9f756675b33b4ae03ca7bc0b2e
AzizDevX <azizkammoun1919@gmail.com>
2025-07-26 17:41:15 +0100

    Updates

--- assets/navbar-C8PVcILz.js
+++ assets/navbar-C8PVcILz.js
@@ -1,4 +1,4 @@
-import{r as i,j as e,L as n}from"./index-dUgwcTQi.js";import{N as k}from"./footer-cqGdDCI0.js";/**
+import{r as i,j as e,L as n}from"./index-BpZo3LQA.js";import{N as k}from"./footer-C3NNNKtL.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
